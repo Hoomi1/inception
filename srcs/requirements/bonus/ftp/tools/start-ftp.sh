6ea0461bffd68e3016c46b92d2ac35d390190b30
Hoomi1 <glebbambam@yandex.ru>
2022-04-06 17:24:00 +0300

    ftp 80% peredacha local->server not work

--- srcs/requirements/bonus/ftp/tools/start-ftp.sh
+++ srcs/requirements/bonus/ftp/tools/start-ftp.sh
@@ -1,19 +1,23 @@
-#!/bin/sh
+# !/bin/sh
 
-if [ ! -f "/etc/vsftpd.conf.bak" ]; then
+useradd -m -s /bin/bash $FTP_USER && echo "$FTP_USER:$FTP_PASS" | chpasswd && mkdir -p /var/www/ftp && "$FTP_USER" | tee -a /etc/vsftpd.userlist
+mkdir -p /var/run/vsftpd/
+mkdir -p /var/run/vsftpd/empty
+# if [ ! -f "/etc/vsftpd.conf.bak" ]; then
 
-	mkdir -p /var/www/html
+# 	mkdir -p /var/www/html
 
-	cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
-	mv /tmp/vsftpd.conf /etc/vsftpd.conf
+# 	cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
+# 	mv vsftpd.conf /etc/vsftpd.conf
 
-	adduser $FTP_USER --disabled-password
-	echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd &> /dev/null
-	chown -R $FTP_USER:$FTP_USER /var/www/html
+# 	adduser $FTP_USER --disabled-password
+# 	echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd &> /dev/null
+# 	chown -R $FTP_USER:$FTP_USER /var/www/html
 
-	echo $FTP_USER | tee -a /etc/vsftpd.userlist &> /dev/null
+# 	echo $FTP_USER | tee -a /etc/vsftpd.userlist &> /dev/null
 
-fi
+# fi
 
-echo "FTP started on :21"
+# echo "FTP started on :21"
+# service vsftpd start
 /usr/sbin/vsftpd /etc/vsftpd.conf
