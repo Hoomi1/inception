7f0a506033ca285292f33dac380e088806bb8720
Hoomi1 <glebbambam@yandex.ru>
2022-04-14 18:39:43 +0300

    Work 100%

--- srcs/requirements/mariadb/tools/db.sh
+++ srcs/requirements/mariadb/tools/db.sh
@@ -1,14 +1,14 @@
 chown -R mysql:mysql /var/lib/mysql
 
+if [ ! -d "/var/lib/mysql/$NAME_DB" ]; then
 service mysql start
-
-mysql -u root --skip-password -e "CREATE DATABASE $NAME_DB;" 
-mysql -u root --skip-password -e "CREATE USER '$USER_DB'@'%' IDENTIFIED BY '$PASSWORD_USER_DB';"
-mysql -u root --skip-password -e "GRANT ALL PRIVILEGES ON $NAME_DB.* TO $USER_DB@'%';"
-mysql -u root --skip-password -e "FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $NAME_DB DEFAULT CHARACTER SET utf8;;" 
+mysql -u root -e "CREATE USER '$USER_DB'@'%' IDENTIFIED BY '$PASSWORD_USER_DB';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $NAME_DB.* TO $USER_DB@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root password $PASSWORD_ROOT_DB
 
 service mysql stop
-
+fi
 usr/bin/mysqld_safe
